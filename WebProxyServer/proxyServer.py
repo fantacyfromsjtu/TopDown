424eeae8b279b7b82469dd4b7fa8265341453ec7
Fantacy <2307068939@qq.com>
2024-08-04 15:18:46 +0800

    fix some bug

--- WebProxyServer/proxyServer.py
+++ WebProxyServer/proxyServer.py
@@ -12,68 +12,80 @@ serverPort = 12000  # The port used by the server
 tcpSerSock.bind(('', serverPort))
 tcpSerSock.listen(10)  # Listen for incoming connections
 
-while 1:
+while True:
     print('Ready to serve...')
 
     tcpCliSock, addr = tcpSerSock.accept()
     print('Received a connection from:', addr)
 
-    message = tcpCliSock.recv(4096).decode()
-    print(message)
-
-    # Extract the filename from the given message
-    url = message.split()[1]
-    filename = url.partition("/")[2]
-    print(filename)
-
-    fileExist = "false"
-    filetouse = "/" + filename.replace("/", "_")
-    print(filetouse)
-
     try:
-        # Check whether the file exists in the cache
-        f = open(filetouse[1:], "r")
-        outputdata = f.read().replace("\r\n", "\r").splitlines(True)
-        fileExist = "true"
-        tcpCliSock.send("HTTP/1.0 200 OK\r\n".encode())
-        tcpCliSock.send("Content-Type:text/html\r\n".encode())
-        for line in outputdata:
-            tcpCliSock.send(line.encode())
-        print('Read from cache')
+        message = tcpCliSock.recv(4096).decode()
+        print(message)
 
-    except IOError:
-        if fileExist == "false":
-            c = socket(AF_INET, SOCK_STREAM)
-            hostn = url.split('/')[2]
-            print(hostn)
+        # Extract the URL and filename from the given message
+        url = message.split()[1]
+        hostn = url.split('/')[2]
+        path = '/' + '/'.join(url.split('/')[3:])
+        filename = url.replace("http://", "").replace("/", "_").replace(":", "_")
+        print(path)
+        print(filename)
 
-            try:
-                # Connect to the target server on port 80
-                c.connect((hostn, 80))
+        fileExist = "false"
+        filetouse = filename
+        print(filetouse)
 
-                # Create and send the request to the target server
-                request_line = f"GET /{'/'.join(filename.split('/')[1:])} HTTP/1.0\r\nHost: {hostn}\r\n\r\n"
-                c.send(request_line.encode())
+        try:
+            # Check whether the file exists in the cache
+            with open(filetouse, "rb") as f:
+                outputdata = f.read().replace(b"\r\n", b"\r")
+                fileExist = "true"
+                tcpCliSock.send("HTTP/1.0 200 OK\r\n".encode())
+                tcpCliSock.send("Content-Type:text/html\r\n".encode())
+                tcpCliSock.send(outputdata)
+            print('Read from cache')
 
-                # Read the response from the target server
-                response = b""
-                while True:
-                    data = c.recv(4096)
-                    if len(data) > 0:
-                        response += data
-                    else:
-                        break
-                tcpCliSock.send(response)
+        except IOError:
+            if fileExist == "false":
+                c = socket(AF_INET, SOCK_STREAM)
+                print(hostn)
 
-                # Cache the response
-                with open(filetouse[1:], "wb") as tmpFile:
-                    tmpFile.write(response.replace(b"\r\n", b"\r"))
+                try:
+                    # Connect to the target server on port 80
+                    c.connect((hostn, 80))
 
-                c.close()
-            except Exception as e:
-                print("Illegal request:", e)
-                tcpCliSock.send("HTTP/1.0 400 Bad Request\r\n\r\n".encode())
-        else:
-            tcpCliSock.send("HTTP/1.0 404 Not Found\r\n".encode())
+                    # Create and send the request to the target server
+                    request_line = f"GET {path} HTTP/1.0\r\nHost: {hostn}\r\n\r\n"
+                    c.send(request_line.encode())
 
-    tcpCliSock.close()
+                    # Read the response from the target server
+                    response = b""
+                    while True:
+                        data = c.recv(4096)
+                        if len(data) > 0:
+                            response += data
+                        else:
+                            break
+                    tcpCliSock.send(response)
+
+                    # Cache the response
+                    with open(filetouse, "wb") as tmpFile:
+                        tmpFile.write(response.replace(b"\r\n", b"\r"))
+
+                    c.close()
+                except Exception as e:
+                    print("Illegal request:", e)
+                    try:
+                        tcpCliSock.send("HTTP/1.0 400 Bad Request\r\n\r\n".encode())
+                    except Exception as send_err:
+                        print("Error sending error message to client:", send_err)
+            else:
+                try:
+                    tcpCliSock.send("HTTP/1.0 404 Not Found\r\n".encode())
+                except Exception as send_err:
+                    print("Error sending 404 message to client:", send_err)
+
+    except Exception as e:
+        print("Error handling request:", e)
+
+    finally:
+        tcpCliSock.close()
